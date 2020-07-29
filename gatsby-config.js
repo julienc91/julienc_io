69e8bef0f54c9211ecbab00d506dafad62ea75cd
julien <git@julienc.io>
2020-07-29 07:45:39 +0200

    redirects not working

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,7 @@ module.exports = {
         icon: 'content/assets/logo512.png'
       }
     },
-    'gatsby-plugin-react-helmet'
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-meta-redirect'
   ]
 }
